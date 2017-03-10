c19392b9ceb36b902d076557b05a25451a115a69
rabrownl <rabrownl@microsoft.com>
2017-03-11 10:20:27 +1100

    initial upload
    adding first quick start templates

---
+++ azure-quickstart-templates/Deploy-ARM-SCOM.ps1
@@ -0,0 +1,15 @@
+﻿# SET THE NAME FOR YOUR RESOURCE GROUP AND VMS.  USE LOWER CASE AND KEEP IT SHORT!  EG PRD123
+
+$demoname = "raflab"
+
+New-AzureRmResourceGroup -Name $demoname -Location "Australia East"
+
+# CREATE DOMAIN CONTROLLER
+    New-AzureRmResourceGroupDeployment -Name $demoname -ResourceGroupName $demoname -TemplateFile "C:\scripts\azure-quickstart-templates\active-directory-new-domain\azuredeploy.json" -TemplateParameterFile "C:\scripts\azure-quickstart-templates\active-directory-new-domain\azuredeploy.parameters.json" -verbose -dnsPrefix "$demoname-ad"
+
+#DOMAIN JOIN VM
+    New-AzureRmResourceGroupDeployment -Name $demoname -ResourceGroupName $demoname -TemplateFile "C:\scripts\azure-quickstart-templates\201-vm-domain-join\azuredeploy.json" -TemplateParameterFile "C:\scripts\azure-quickstart-templates\201-vm-domain-join\azuredeploy.parameters.json" -verbose -dnslabelprefix "$demoname-sql"
+
+#DOMAIN JOIN VM
+    New-AzureRmResourceGroupDeployment -Name $demoname -ResourceGroupName $demoname -TemplateFile "C:\scripts\azure-quickstart-templates\201-vm-domain-join\azuredeploy.json" -TemplateParameterFile "C:\scripts\azure-quickstart-templates\201-vm-domain-join\azuredeploy.parameters.json" -verbose -dnslabelprefix "$demoname-scom"
+
