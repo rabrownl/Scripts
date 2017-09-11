50fb04dd9419f12c10540c169be53c05e74e09e8
rabrownl <rabrownl@microsoft.com>
2017-09-11 10:49:10 +1000

    updated to share same storage account name

--- azure-quickstart-templates/Deploy-ARM.ps1
+++ azure-quickstart-templates/Deploy-ARM.ps1
@@ -1,5 +1,9 @@
 ﻿# SET THE NAME FOR YOUR RESOURCE GROUP AND VMS.  USE LOWER CASE AND KEEP IT SHORT!  EG PRD123
 
+$subscriptionid = "c73e33a6-6855-4123-a017-d95432c640ce"
+
+Select-AzureRmSubscription -SubscriptionId $subscriptionid
+
 $demoname = "sealab"
 
 $domainname = "sea.lab"
@@ -11,15 +15,17 @@ $password = ConvertTo-SecureString "P@55w0rd12345" -AsPlainText -Force
 New-AzureRmResourceGroup -Name $demoname -Location "Southeast Asia"
 
 # CREATE DOMAIN CONTROLLER
-    New-AzureRmResourceGroupDeployment -Name $demoname -ResourceGroupName $demoname -TemplateFile "C:\scripts\scripts\azure-quickstart-templates\active-directory-new-domain\azuredeploy.json" -TemplateParameterFile "C:\scripts\scripts\azure-quickstart-templates\active-directory-new-domain\azuredeploy.parameters.json" -verbose `
+ $output = New-AzureRmResourceGroupDeployment -Name $demoname -ResourceGroupName $demoname -TemplateFile "C:\scripts\scripts\azure-quickstart-templates\active-directory-new-domain\azuredeploy.json"`
+    -TemplateParameterFile "C:\scripts\scripts\azure-quickstart-templates\active-directory-new-domain\azuredeploy.parameters.json" -verbose `
     -dnsPrefix "$demoname-ad" -adminUsername $username -adminPassword $password -domainName $domainname -adVMName "SEADC01"
 
 
 #NEW SQL Server DOMAIN JOIN
     New-AzureRmResourceGroupDeployment -Name $demoname -ResourceGroupName $demoname -TemplateFile "C:\scripts\scripts\azure-quickstart-templates\new-sql-domain-join\azuredeploy.json" `
-    -vmName "SEASQL01" -domainPassword $domainpassword -vmAdminUsername $username -domainUsername $username -domainToJoin $demoname -vmAdminPassword $password -verbose 
+    -vmName "seasql01" -domainPassword $password -vmAdminUsername $username -domainUsername $username -domainToJoin $domainname -vmAdminPassword $password `
+    -existingVNETName "adVNET" -existingSubnetName "adSubnet" -storageAccountName $output.outputs.Values.Value -verbose 
 
-    
+
 #DOMAIN JOIN VM
 New-AzureRmResourceGroupDeployment -Name $demoname -ResourceGroupName $demoname -TemplateFile "C:\scripts\azure-quickstart-templates\201-vm-domain-join\azuredeploy.json" `
 -TemplateParameterFile "C:\scripts\azure-quickstart-templates\201-vm-domain-join\azuredeploy.parameters.json" -verbose `
